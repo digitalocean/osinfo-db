b8fecd04e49f2414406f4ef1e09766e6104f502d
Guido Günther <agx@sigxcpu.org>
2016-11-06 17:01:09 +0100

    Add autopkgtest

---
+++ debian/tests/test_jessie.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import unittest
+
+import gi
+gi.require_version('Libosinfo', '1.0')
+from gi.repository import Libosinfo as osinfo;
+
+class TestJessie(unittest.TestCase):
+
+    def setUp(self):
+        self.loader = osinfo.Loader()
+        self.loader.process_default_path()
+        self.db = self.loader.get_db()
+
+    def testVirtio(self):
+        """
+        Make sure jessie has vitio block and net
+        """
+        osid = "http://debian.org/debian/8"
+        os = self.db.get_os(osid)
+        devs = os.get_all_devices()
+        devnames = []
+
+        for idx in range(devs.get_length()):
+            devnames.append(devs.get_nth(idx).get_name())
+
+        self.assertIn("virtio-net", devnames)
+        self.assertIn("virtio-block", devnames)
+
