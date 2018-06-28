fd17d3cf67d468c261153900e47687e40fb27531
Guido Günther <agx@sigxcpu.org>
2018-06-28 09:06:53 +0200

    New upstream version 0.20180628

--- prepare-release.sh
+++ prepare-release.sh
@@ -4,7 +4,7 @@ set -e
 set -v
 
 test -n "$1" && RESULTS=$1 || RESULTS=results.log
-: ${AUTOBUILD_INSTALL_ROOT=$HOME/builder}
+INSTALL_ROOT=$HOME/builder
 
 # Make things clean.
 make -k clean
@@ -22,18 +22,10 @@ case $MAKEFLAGS in
 esac
 
 make
-make install DESTDIR="$AUTOBUILD_INSTALL_ROOT" OSINFO_DB_TARGET="--system"
-
-if [ -n "$AUTOBUILD_COUNTER" ]; then
-  EXTRA_RELEASE=".auto$AUTOBUILD_COUNTER"
-else
-  NOW=`date +"%s"`
-  EXTRA_RELEASE=".$USER$NOW"
-fi
+make install DESTDIR="$INSTALL_ROOT" OSINFO_DB_TARGET="--system"
 
 if [ -f /usr/bin/rpmbuild ]; then
   rpmbuild --nodeps \
-     --define "extra_release $EXTRA_RELEASE" \
      --define "_sourcedir `pwd`" \
      -ba --clean osinfo-db.spec
 fi
@@ -41,7 +33,6 @@ fi
 if test -x /usr/bin/i686-w64-mingw32-gcc && test -x /usr/bin/x86_64-w64-mingw32-gcc ; then
   if test -f /usr/bin/rpmbuild ; then
     rpmbuild --nodeps \
-       --define "extra_release $EXTRA_RELEASE" \
        --define "_sourcedir `pwd`" \
        -ba --clean mingw-osinfo-db.spec
   fi
